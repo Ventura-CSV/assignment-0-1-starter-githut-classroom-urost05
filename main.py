4b20fc0fde54ee8965ecd770925ebbf6725d4952
urost05 <142646381+urost05@users.noreply.github.com>
2023-08-19 20:40:22 -0700

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():
     print('hello world')
     print('CS7: Python Programming')
 
-
+link(https://github.com/Ventura-CSV/assignment-0-1-starter-githut-classroom-urost05) 
 
 if __name__ == '__main__':
     main()
