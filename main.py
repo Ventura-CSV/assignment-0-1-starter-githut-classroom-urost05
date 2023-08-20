b9b585038c90a237b75ce233cdb6541f7ae73c39
urost05 <ull.rosten@gmail.com>
2023-08-19 19:02:42 -0700

    Create main.py

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ def main():
     # make your code below
     # print('Hello World')
     ##############################
+    print('hello world')
+    print('CS7: Python Programming')
+
 
 
 if __name__ == '__main__':
